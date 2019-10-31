72a559fac3072c584bd56a5d2dab7f77718d0db1
Dave Degeatano <davey.dee33@gmail.com>
2019-10-31 15:16:11 -0700

    Navbar and 2 pages for routing

---
+++ client/src/components/layout/Navbar.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+
+const Navbar = ({ title, icon }) => {
+  return (
+    <div className="navbar bg-primary">
+      <h1>
+        <Link to="/">
+          <i className={icon} /> {title}
+        </Link>
+      </h1>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+Navbar.propTypes = {
+  title: PropTypes.string.isRequired,
+  icon: PropTypes.string
+};
+
+Navbar.defaultProps = {
+  title: 'MemBox',
+  icon: 'fas fa-box-open'
+};
+
+export default Navbar;
---
+++ client/src/components/pages/About.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <div>
+      <h1>About This App</h1>
+    </div>
+  );
+};
+
+export default About;
