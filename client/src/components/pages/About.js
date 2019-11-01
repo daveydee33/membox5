ca322d6d5da4ce540c0f03827c3b165dafdfa44a
Dave Degeatano <davey.dee33@gmail.com>
2019-10-31 22:38:56 -0700

    updated About page

--- client/src/components/pages/About.js
+++ client/src/components/pages/About.js
@@ -4,6 +4,25 @@ const About = () => {
   return (
     <div>
       <h1>About This App</h1>
+      <br />
+      <p>
+        Use this to save new words, phrases, snippets that you are learning.
+        Save them and take notes in the text area. Save phrases that you want to
+        remember to review or that you want to reference again later.
+      </p>
+      <br />
+      <p>
+        Think of each item as a card that should just hold a snippet of
+        information -- like a Post-It note, or a flash card. On one side of your
+        card you could have a snippet tile, and the back side you could have the
+        details.
+      </p>
+      <br />
+      <p>
+        Then you can use this app like a box full of your index card notes, with
+        a quick way to organize, search, add, edit, delete, and categorize your
+        snippet cards.
+      </p>
     </div>
   );
 };
