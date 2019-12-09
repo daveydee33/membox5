0da54578fa3cb2c123b9c73d17ca621fb2f5b7c5
Dave Degeatano <davey.dee33@gmail.com>
2019-12-09 21:38:02 +0800

    WIP add tags to UI

---
+++ client/src/constants.js
@@ -0,0 +1,6 @@
+export const tagOptions = [
+  { value: 'indonesian', label: 'Indonesian' },
+  { value: 'khmer', label: 'Khmer' },
+  { value: 'spanish', label: 'Spanish' },
+  { value: 'portuguese', label: 'Portuguese' },
+];
