4203e6d9182eb36b9644d24b2a114515b0323279
Dave Degeatano <davey.dee33@gmail.com>
2020-03-01 20:37:21 +0800

    update http-proxy-middleware and fix breaking changes

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -1,9 +1,9 @@
-const proxy = require('http-proxy-middleware');
+const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function(app) {
   app.use(
-    proxy(['/api', '/auth'], {
-      target: 'http://localhost:5000'
-    })
+    createProxyMiddleware(['/api', '/auth'], {
+      target: 'http://localhost:5000',
+    }),
   );
 };
